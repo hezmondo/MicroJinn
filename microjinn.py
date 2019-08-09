dd9a3233365650ac60162521865ebf71c58f20a4
jnbarchan <jnbarchan@users.noreply.github.com>
2019-08-09 11:55:54 +0100

    Getting going

--- microjinn.py
+++ microjinn.py
@@ -6,6 +6,6 @@ from app.models import User, Rent
 def make_shell_context():
     return {'db': db, 'User': User, 'Rent': Rent}
 
+
 if __name__ == '__main__':
     app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
-
