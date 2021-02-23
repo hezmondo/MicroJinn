267b3a4c7d433998033f3267a05b50e4610e5e34
Sam-Morgan <s.andrew.morgan@gmail.com>
2021-02-23 13:21:07 +0000

    New ELC rent screen with modal popup

--- app/static/mjinn.js
+++ app/static/mjinn.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("#save_delete").hide();
-    $("#more_buttons").hide();
+    $("#more_views").hide();
     $("#xalloc").hide();
     $("#show-xalloc").click(function(){
         $("#xalloc").toggle();
@@ -34,6 +34,7 @@ $(document).ready(function(){
             $("#save_delete").hide();
         }
     });
+    //add tick icon on click to toggle button
     $(".btn-check").click(function(){
         if($(".btn-tog").hasClass('check')){
             $(".btn-tog").removeClass("check");
@@ -42,14 +43,16 @@ $(document).ready(function(){
             $(".btn-tog").addClass("check");
         }
     });
+    //show more details and scroll to bottom
     $("#more_less").click(function(){
-        if ($(this).text() == "more") {
-            $(this).text("less");
-            $("#more_buttons").show();
+        if ($(this).text() == "view more details") {
+            $(this).text("view fewer details");
+            $("#more_views").show();
+            $('html, body').scrollTop( $(document).height() - $(window).height() );
             }
         else {
-            $(this).text("more");
-            $("#more_buttons").hide();
+            $(this).text("view more details");
+            $("#more_views").hide();
         }
     });
     $(".custom-file-input").on("change", function() {
@@ -60,12 +63,14 @@ $(document).ready(function(){
         var mysave = $('#doc_html').html();
         $('#xinput').val(mysave);
     });
+    //copy address field and separate each line
     $('#address_fields').click(function() {
         var addr = $('#address_fields option:selected').text();
         var addr_split = addr.replace(/,/g, "<br />");
         $('#addr_span').html(addr_split);
         $('#addr_span').append("<br />");
     });
+    //save pr - collect address to put in summary
     $('.save_pr').click(function() {
         var pr_block = $('#doc_html').html();
         $('#pr_block').val(pr_block);
@@ -73,16 +78,24 @@ $(document).ready(function(){
         var pr_addr_strip = pr_addr.replace(/[^\x20-\x7E]/gmi, "");
         $('#pr_addr').val(pr_addr_strip);
     });
-
-    resize_view();
-    $(window).resize(function(){
-        resize_view();
+    //copy text area in rent screen on click
+    $('.copyable').click(function(e) {
+        if ( $(this).is('[readonly]') ) {
+            var element = $(this);
+            copyToClipboard(this);
+            $('#exampleModal').modal();
+            setTimeout(function() {$('#exampleModal').modal('hide');}, 1000);
+        }
     });
-      var loc = window.location.pathname;
-
-   $('#nav_bar').find('a').each(function() {
-     $(this).toggleClass('active', $(this).attr('href') == loc);
-  });
+    //allow display and navbar to change for smaller screens / phones
+    resizeView();
+    $(window).resize(function(){
+        resizeView();
+    });
+    var loc = window.location.pathname;
+    $('#nav_bar').find('a').each(function() {
+        $(this).toggleClass('active', $(this).attr('href') == loc);
+    });
 
 });
 $(function() {
@@ -91,25 +104,33 @@ $(function() {
     $('#sidebar, #content').toggleClass('active');
   });
 });
+//allows for bootstrap style tooltips
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
 
-function resize_view() {
-            var viewportwidth;
-        if(typeof window.innerWidth!='undefined'){
-              viewportwidth=window.innerWidth;
-        }
-       $('#nav_aside').removeClass("fixed-top");
-       $('#nav_aside').removeClass("nav-display");
-       $('#nav_bar').removeClass("navbar-expand");
+//allow display and navbar to change for smaller screens / phones
+function resizeView() {
+        var viewportwidth;
+    if(typeof window.innerWidth!='undefined'){
+          viewportwidth=window.innerWidth;
+    }
+   $('#nav_aside').removeClass("fixed-top");
+   $('#nav_aside').removeClass("nav-display");
+   $('#nav_bar').removeClass("navbar-expand");
 
-       if(viewportwidth >= 768){
-           $('#nav_aside').addClass("fixed-top");
-           $('#nav_aside').addClass("nav-display");
-           $('#nav_bar').removeClass("navbar-expand");
-       }
-       else {
-           $('#nav_bar').addClass("navbar-expand");
-        }
+   if(viewportwidth >= 768){
+       $('#nav_aside').addClass("fixed-top");
+       $('#nav_aside').addClass("nav-display");
+       $('#nav_bar').removeClass("navbar-expand");
+   }
+   else {
+       $('#nav_bar').addClass("navbar-expand");
+    }
+}
+//function to copy text from text area (in rent screen) to clipboard
+function copyToClipboard(element) {
+  element.select();
+  element.setSelectionRange(0, 99999)
+  document.execCommand("copy");
 }
