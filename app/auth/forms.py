18af3a2986a607084a337cbd1d6bd55fb0f7fc9a
Sam-Morgan <s.andrew.morgan@gmail.com>
2021-02-24 19:37:43 +0000

    Add recent rent to nav, more rent ux changes

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,7 +5,7 @@ from app.models import User
 
 
 class EditProfileForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    _username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me',
                              validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
