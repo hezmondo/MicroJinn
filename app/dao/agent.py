a3c41ad768e9b95ca521a89e1c9009d06f947196
Sam-Morgan <s.andrew.morgan@gmail.com>
2021-03-24 13:14:39 +0000

    Agent code refactored. Modals have auto focus.

--- app/dao/agent.py
+++ app/dao/agent.py
@@ -5,6 +5,12 @@ from app.dao.database import commit_to_database
 from app.models import Agent, Headrent, Rent
 
 
+def add_agent(agent):
+    db.session.add(agent)
+    db.session.flush()
+    return agent.id
+
+
 def get_agents_set(filter):
     return db.session.query(Agent).filter(*filter).all()
 
@@ -15,32 +21,3 @@ def get_agent(agent_id):
 
 def get_agent_id(agent_detail):
     return db.session.query(Agent).filter_by(detail=agent_detail).first()
-
-
-def get_agent_rents(agent_id):
-    return db.session.query(Rent).filter_by(agent_id=agent_id).options(load_only('id', 'rentcode', 'tenantname')).all()
-
-
-def get_agent_headrents(agent_id):
-    return db.session.query(Headrent).filter_by(agent_id=agent_id).options(load_only('id', 'code', 'propaddr')).all()
-
-
-def post_agent(agent, agent_id, rent_id):
-    try:
-        db.session.add(agent)
-        db.session.flush()
-        message = "Agent details updated successfully!"
-        agent_id = agent.id
-        if rent_id != 0:
-            rent = Rent.query.get(rent_id)
-            rent.agent_id = agent_id
-            rent.mailto_id = 1
-            db.session.add(rent)
-            message += " Mail has been set to agent. Please review this rent\'s mailto details."
-        commit_to_database()
-        # update the user recent_agents to include a newly created agent (after we have done the main database work)
-        pop_idlist_recent("recent_agents", agent_id)
-    except Exception as ex:
-        message = f"Update agent failed. Error:  {str(ex)}"
-
-    return agent_id, message
