3ddde3ee5640ce5f4cc372ec13096ae81ba12a3b
jnbarchan <jnbarchan@users.noreply.github.com>
2021-03-30 11:33:15 +0100

    HrStatuses accessible as "global"

--- mjinn.py
+++ mjinn.py
@@ -1,9 +1,13 @@
 import datetime
-from app import create_app, db
+from app import app, init_app, db
 from app.models import User
 
 
-app = create_app()
+# Call `init_app()` in `__init__.py` here
+# We declare and create the global `app` instance in `__init__.py`
+# This allows us to access it, and use function decorations like `@app.context_processor`, from other modules
+# which would not be the case if we created `app` from this module
+init_app()
 
 
 @app.shell_context_processor
